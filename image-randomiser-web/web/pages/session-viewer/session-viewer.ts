023b4f09bb0e934151b4f9cee86baf4e34ff900c
risonakamo <hmykanae@gmail.com>
2025-04-19 11:46:04 -0400

    purge web repo for submod convert

--- image-randomiser-web/web/pages/session-viewer/session-viewer.ts
+++
@@ -1,9 +0,0 @@
-import {mount} from "svelte";
-import SessionViewer from "./session-viewer.svelte";
-
-function main():void
-{
-    mount(SessionViewer,{target:document.body});
-}
-
-window.onload=main;
